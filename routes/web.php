212306b5852d44035b8ef195488a51b315a9ac46
krish <vora76637@gmail.com>
2026-06-09 09:25:46 +0530

    Add Idea and Step models with factories, requests, and policies

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisteredUserController;
 
 Route::get('/', fn () => view('welcome'));
+
+Route::get('/register', [RegisteredUserController::class, 'create'])->name('register')->middleware('guest');
+Route::post('/register', [RegisteredUserController::class, 'store'])->middleware('guest');
+
+Route::get('/login', [SessionsController::class, 'create'])->name('login')->middleware('guest');
+Route::post('/login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('/logout', [SessionsController::class, 'destroy'])->middleware('auth');
